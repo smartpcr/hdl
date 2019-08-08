9e0f66933e6922eac5c8d37aed61284b0a4c94b4
lingxd <lingxd@gmail.com>
2019-08-08 02:01:09 -0700

    submodule

--- sync.ps1
+++ sync.ps1
@@ -1,2 +1,15 @@
+
+$ErrorActionPreference = "Stop"
+Set-StrictMode -Version Latest
+
+$gitRootFolder = if ($PSScriptRoot) { $PSScriptRoot } else { Get-Location }
+while ((-not (Test-Path (Join-Path $gitRootFolder ".git"))) -and (-not $gitRootFolder.ToUpper().EndsWith("HDL"))) {
+    $gitRootFolder = Split-Path $gitRootFolder -Parent
+}
+
 fab install ./infra
-Move-Item -Path ./infra/components -Destination ./manifest/infra -Force 
+Move-Item -Path ./infra/components -Destination ./manifest/infra -Force
+Set-Location (Join-Path $gitRootFolder "manifest" "infra")
+git add .
+git commit -m "audo sync"
+git push 
