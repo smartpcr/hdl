6d4219d0afd464fee87dc96faff4065c17ffe4ae
xiaodoli <xiaodoli@microsoft.com>
2019-08-08 14:29:52 -0700

    sync, split config

--- scripts/sync-infra.ps1
+++ scripts/sync-infra.ps1
@@ -18,7 +18,7 @@ if (Test-Path $destinationComponentFolder) {
     Remove-Item $destinationComponentFolder -Recurse -Force
 }
 if (Test-Path (Join-Path $infraFolder "generated")) {
-    Move-Item -Path (Join-Path $infraFolder "generated") -Destination (Join-Path $GitOpsRepoFolder "infra") -Force
+    Copy-Item -Path (Join-Path $infraFolder "generated") -Destination (Join-Path $GitOpsRepoFolder "infra") -Force -Recurse
 }
 
 Write-Host "sync infra/config"
@@ -27,7 +27,7 @@ if (Test-Path $destinationComponentFolder) {
     Remove-Item $destinationComponentFolder -Recurse -Force
 }
 if (Test-Path (Join-Path $infraFolder "config")) {
-    Move-Item -Path (Join-Path $infraFolder "config") -Destination (Join-Path $GitOpsRepoFolder "infra") -Force
+    Copy-Item -Path (Join-Path $infraFolder "config") -Destination (Join-Path $GitOpsRepoFolder "infra") -Force -Recurse
 }
 
 Set-Location $GitOpsRepoFolder
